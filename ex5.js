b1df44aca3d3c6161a09f9d5c5ca7d910f433fe4
Raphael Lins <rapharell06@gmail.com>
2022-09-15 19:49:49 -0300

    Add files via upload

---
+++ ex5.js
@@ -0,0 +1,4 @@
+let salario = Number(prompt('Digite seu sálario mensal: '))
+let reajuste = salario * 0.10
+let salnovo = salario + reajuste
+alert('O seu sálario com o reajuste é de: ' + salnovo +' Reais!')
